9a73e90c5b2d03a4a5237cf14d080844913f4fcb
Elish <medioni@g.jct.ac.il>
2023-03-05 19:01:56 +0200

    commit elisheva

--- DB.js
+++ DB.js
@@ -1,9 +1,11 @@
 //define user class with name and movies array
 class User {
-    constructor(username,mail,password,movies) {
+    constructor(username,mail,password,time,trials,movies) {
         this.name = username;
         this.mail = mail;
         this.password = password;
+        this.time = time;
+        this.trials = trials;
         this.movies = movies;
     }
 }
@@ -18,19 +20,58 @@ class Movie {
     }
 }
 
+//window.localStorage.clear();
 
-function add_user(user_json){
-    var new_user = JSON.parse(user_json);
-    localStorage.setItem('user', JSON.stringify(new_user));
+//2 arrays to store the users and movies
+var users = [];
+var movies = [];
+
+
+
+//function to get the users array from local storage
+function get_users(){
+    var users = JSON.parse(localStorage.getItem('users'));
+    return users;
 }
 
-//doit etre add_movie et pas add_to_basket - A CHANGER
-function add_to_basket(movie_json, user_json) {
+//function to get the movies array from local storage
+function get_movies(){
+    var movies = JSON.parse(localStorage.getItem('movies'));
+    return movies;
+}
 
-    var new_user = JSON.parse(user_json);
-    var new_movie = JSON.parse(movie_json);
+function get_current_user(){
+    return localStorage.getItem('CurrentUser');
+}
 
-    new_user.movies.push(new_movie);
+function get_current_movie(){
+    return localStorage.getItem('CurrentMovie');
+}
+
+function update_current_user(user){
+    localStorage.setItem('CurrentUser', JSON.stringify(user));
+}
+
+function update_current_movie(movie){
+    localStorage.setItem('CurrentMovie', JSON.stringify(movie));
+}
+
+function delete_current_user(){
+    localStorage.removeItem('CurrentUser');
+}
+
+
+function add_user(user){
+    users.push(user);
+    localStorage.setItem('user', JSON.stringify(user));
+    localStorage.setItem('users', JSON.stringify(users));
+}
+
+function add_to_basket() {
+    var movie = get_current_movie();
+    var new_user = get_current_user();
+    //var new_movie = JSON.parse(movie_json);
+    new_user.movies.push(movie);
 
     //save the user object in local storage
     localStorage.setItem('user', JSON.stringify(new_user));
@@ -48,5 +89,7 @@ function delete_movie(movie_json, user_json) {
     //remove the movie from the user from local storage
     localStorage.setItem('user', JSON.stringify(new_user));
 }
+
+
 
 //faire fonction update user, update movie, delete user, get_user, get_movie
--- server.js
+++ server.js
@@ -3,50 +3,75 @@
 
 class Server{
     static carry_request(body, obj){
-        if(obj.method == 'POST'){
-            var obj = JSON.parse(body);
-            if(obj.type == 'User'){
-                add_user(obj);
-                obj.status = 200;
-                obj.ReadyState = ReadyState.DONE;
-                return obj;
-            }
-            else if(obj.type == 'Movie'){
-                add_to_basket(obj);
-                return obj;
-            }
+        //POST
+        if(obj.method == 'POST' && obj.url =="http://localhost:3000/add_user"){
+            //var new_user = JSON.parse(body);
+            add_user(body);
+            obj.status = 200;
+            obj.ReadyState = ReadyState.DONE;
+            return obj;
         }
-        else if(obj.method == 'GET'){
-            var obj = JSON.parse(body);
-            if(obj.type == 'User'){
-                return get_user();
-            }
-            else if(obj.type == 'Movie'){
-                return get_movie();
-            }
+        else if(obj.method == 'POST' && obj.url =="http://localhost:3000/add_movie"){
+            //var new_movie = JSON.parse(body);
+            add_to_basket(body);
+            obj.status = 200;
+            obj.ReadyState = ReadyState.DONE;
+            return obj;
         }
-        else if(obj.method == 'PUT'){
-            var obj = JSON.parse(body);
-            if(obj.type == 'User'){
-                update_user(obj);
-                return obj;
-            }
-            else if(obj.type == 'Movie'){
-                update_movie(obj);
-                return obj;
-            }
+        //GET
+        else if(obj.method == 'GET' && obj.url =="http://localhost:3000/get_user"){
+            var objet = JSON.parse(body);
+            obj.status = 200;
+            obj.ReadyState = ReadyState.DONE;
+            obj.response = get_current_user();
+            return obj;
         }
-        else if(obj.method == 'DELETE'){
-            var obj = JSON.parse(body);
-            if(obj.type == 'User'){
-                delete_user(obj);
-                return obj;
-            }
-            else if(obj.type == 'Movie'){
-                delete_movie(obj);
-                return obj;
-            }
+
+        else if (obj.method == 'GET' && obj.url =="http://localhost:3000/get_users"){
+            obj.response = get_users();
+            return obj;
+        }
+
+        else if(obj.method == 'GET' && obj.url =="http://localhost:3000/get_movies"){
+            obj.response = get_movies();
+            return obj;
+        }
+        else if(obj.method == 'GET' && obj.url =="http://localhost:3000/get_movie"){
+            var movie = JSON.parse(body);
+            obj.status = 200;
+            obj.ReadyState = ReadyState.DONE;
+            obj.response = get_movie(movie);
+            return obj;
+        }
+        //PUT
+        else if(obj.method == 'PUT' && obj.url =="http://localhost:3000/update_current_user"){
+            update_current_user(body);
+            obj.status = 200;
+            obj.ReadyState = ReadyState.DONE;
+            return obj;
+        }
+        else if(obj.method == 'PUT' && obj.url =="http://localhost:3000/update_current_movie"){
+            update_current_movie(body);
+            obj.status = 200;
+            obj.ReadyState = ReadyState.DONE;
+            return obj;
+        }
+
+        //DELETE
+        else if(obj.method == 'DELETE' && obj.url =="http://localhost:3000/delete_current_user"){
+            delete_current_user();
+            obj.status = 200;
+            obj.ReadyState = ReadyState.DONE;
+            return obj;
+        }
+        else if(obj.method == 'DELETE' && obj.url =="http://localhost:3000/delete_movie"){
+            delete_movie(body);
+            obj.status = 200;
+            obj.ReadyState = ReadyState.DONE;
+            return obj;
         }
 
     }
+    
+
 }
